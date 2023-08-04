ebd0785292e99a06e8a380d25450b5b03006d0eb
Julien <julien@ripouteau.com>
2023-08-04 11:41:18 +0200

    wip

--- tests/cache_manager.spec.ts
+++ tests/cache_manager.spec.ts
@@ -1,20 +1,50 @@
-// import { test } from '@japa/runner'
-// import { CacheManager } from '../src/cache_manager.js'
-// import { REDIS_CREDENTIALS } from '../test_helpers/index.js'
-// import EventEmitter from 'node:events'
-// import { Redis } from '../src/drivers/redis.js'
+import Emittery from 'emittery'
+import { test } from '@japa/runner'
+import EventEmitter from 'node:events'
 
-// test.group('Cache manager', () => {
-//   test('Create an instance of driver', ({ assert }) => {
-//     // const emitter = new EventEmitter()
-//     // const manager = new CacheManager(
-//     //   {
-//     //     default: 'redis',
-//     //     list: {
-//     //       redis: () => new Redis({ connection: REDIS_CREDENTIALS, defaultTtl: 1000 }),
-//     //     },
-//     //   },
-//     //   emitter
-//     // )
-//   })
-// })
+import { CacheManager } from '../src/cache_manager.js'
+import { Lru } from '../src/drivers/lru.js'
+
+test.group('Cache Manager', () => {
+  test('should accept EventEmitter or Emittery', async () => {
+    // This test only rely type-checking
+    new CacheManager({ default: 'memory', list: { memory: () => ({}) as any } }, new EventEmitter())
+    new CacheManager({ default: 'memory', list: { memory: () => ({}) as any } }, new Emittery())
+  })
+
+  test('Subscribe to an event', async ({ assert }) => {
+    assert.plan(2)
+
+    const manager = new CacheManager({
+      default: 'memory',
+      list: {
+        memory: () => new Lru({ maxSize: 100, ttl: 10000, prefix: 'test' }),
+      },
+    })
+
+    manager.on('cache:hit', (event) => {
+      assert.equal(event.key, 'foo')
+      assert.equal(event.value, 'bar')
+    })
+
+    await manager.set('foo', 'bar')
+    await manager.get('foo')
+  })
+
+  test('Unsubscribe from an event', async ({ assert }) => {
+    const manager = new CacheManager({
+      default: 'memory',
+      list: {
+        memory: () => new Lru({ maxSize: 100, ttl: 10000, prefix: 'test' }),
+      },
+    })
+
+    const listener = () => assert.fail()
+
+    manager.on('cache:hit', listener)
+    manager.off('cache:hit', listener)
+
+    await manager.set('foo', 'bar')
+    await manager.get('foo')
+  })
+})
