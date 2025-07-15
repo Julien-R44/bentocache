48498799423d903d86fba5cf990a24b018b77abb
zhenwu.li <zhenwu.li@pixocial.com>
2025-07-15 16:43:06 +0800

    chore: export `BentoStore` for type use

--- packages/bentocache/index.ts
+++ packages/bentocache/index.ts
@@ -1,3 +1,4 @@
 export { errors } from './src/errors.js'
 export { BentoCache } from './src/bento_cache.js'
 export { bentostore } from './src/bento_store.js'
+export { BentoStore } from './src/bento_store.js'
